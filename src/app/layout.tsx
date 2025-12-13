694861d36a6a6d4763d15ef6fde7d50db6babf7a
Tayen15 <tiantiantitik@gmail.com>
2025-12-13 09:30:53 +0700

    feat: Initialize Next.js root layout with global styles, metadata, favicons, and core UI components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,53 @@ import FloatingChat from "@/components/FloatingChat";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const metadataBase = new URL("https://pemira-sttnf.vercel.app");
+const metadataTitle = "#AyoVote | PEMILIHAN RAYA MAHASISWA STTNF 2025";
+const metadataDescription = "Pemilihan Raya Mahasiswa STT Terpadu Nurul Fikri atau PEMIRA 2025 adalah acara pemilihan mahasiswa STTNF untuk memilih calon PRESMA (Presiden Mahasiswa) dan WAKIL PRESMA (Wakil Presiden Mahasiswa) dari STTNF untuk masa 2025-2026.";
+
 export const metadata: Metadata = {
-    title: "#AyoVote | PEMILIHAN RAYA MAHASISWA STTNF 2025",
-    description: "Pemilihan Raya Mahasiswa STT Terpadu Nurul Fikri",
+    title: metadataTitle,
+    description: metadataDescription,
     icons: {
         icon: "/icons/favicon.ico",
         apple: "/icons/apple-touch-icon.png"
+    },
+    keywords: [
+        "pemira sttnf",
+        "pemira 2025",
+        "pemilihan raya mahasiswa website",
+        "pemilihan raya sttnf",
+        "pemilihan raya sttnf 2025",
+        "stt terpadu nurul fikri",
+        "pemilihan raya sttnf 2025"
+    ],
+    openGraph: {
+        title: metadataTitle,
+        description: metadataDescription,
+        siteName: metadataTitle,
+        url: metadataBase,
+        type: "website",
+        images: [
+            {
+                url: "/icons/favicon.ico",
+                width: 1200,
+                height: 630,
+                alt: metadataTitle
+            }
+        ]
+    },
+    twitter: {
+        title: metadataTitle,
+        description: metadataDescription,
+        card: "summary_large_image",
+        images: [
+            {
+                url: "/icons/favicon.ico",
+                width: 1200,
+                height: 630,
+                alt: metadataTitle
+            }
+        ]
     }
 };
 
